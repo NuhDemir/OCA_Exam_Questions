3da5bb75ec90b8be18717d1e00f09fc77a7ce8aa
NuhDemir <drmaa217@gmail.com>
2024-06-29 20:18:21 +0300

    Answer 36

---
+++ Java_Basics/src/forecast/Weather.java
@@ -0,0 +1,9 @@
+package forecast;
+
+public class Weather {
+private static boolean heatWave = true;
+public static void main(String[] args) {
+	boolean heatWave = false;
+	System.out.println(heatWave);
+}
+}
